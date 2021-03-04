49d17ca90c8530ad1edb14cfa9420ee21e2ffa2c
Andrey <k18041990@gmail.com>
2021-03-04 23:08:31 +0700

    my first project for lesson 1

--- lesson1/main.swift
+++ lesson1/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+// Квадратное уравнение
+// d*x2 = e
+let d = 12
+let e = 48
 
+let x = Int(sqrt(Double(e/d)))
+
+// Найти площадь, периметр и гипотенузу
+
+let a = 10.0
+let b = 25.0
+let square = a*b/2
+let c = Double(String(format: "%.2f", sqrt(pow(a, 2) + pow(b, 2))))!//гипотенуза
+let perimetr = a+b+c
+
+// Сумма вклада
+let percent = 17.0
+var deposit = 100000.0
+let years = 5
+var i = 0
+while (i < years){
+    deposit += deposit * percent/100
+    print(deposit)
+    i += 1
+}
